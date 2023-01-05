3c4855ee560f3b13f74cdc9c982d08f9987e108f
Belen <gonzaleznovoa.b@protonmail.com>
2023-01-05 18:13:58 +0100

    first commit

---
+++ js_fundamentals_part1/script_01.js
@@ -0,0 +1,9 @@
+let country = "France";
+let continent = "Europe";
+let population = "67,5";
+console.log(`I live in ${country} which is located in ${continent} and has ${population} millions of habitants`);
+
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
