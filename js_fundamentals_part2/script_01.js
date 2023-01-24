44e3df54ff69f6f878fedb475e41785efd64a493
Belen <gonzaleznovoa.b@protonmail.com>
2023-01-24 14:16:34 +0100

    adding function declaration vs expression lecture

--- js_fundamentals_part2/script_01.js
+++ js_fundamentals_part2/script_01.js
@@ -12,3 +12,30 @@ console.log(froggy);
 
 const chorizo = describeCountry('Spain', 47, 'Madrid');
 console.log(chorizo);
+
+//Lecture Functions declarations vs expressions
+
+//Function declarations :
+
+const worldPopulation = 7900;
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+const percentageChina = percentageOfWorld1(1441);
+console.log(percentageChina);
+
+const percentageSpain = percentageOfWorld1(47);
+console.log(percentageSpain);
+
+const percentageIndia = percentageOfWorld1(1408);
+console.log(percentageIndia);
+
+// Function expression :
+
+const percentageOfWorld2 = function calPercentage(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageBrazil = percentageOfWorld2(2143);
+console.log(percentageBrazil);
